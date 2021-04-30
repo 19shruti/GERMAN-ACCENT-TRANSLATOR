9e54fff1c812e6ff802770032f3ce0cc1f299368
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-04-30 16:50:52 +0530

    serveradded

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputBox=document.querySelector("#OUTPUT");
+var serverURL=https://orthosie-old-english-translator-v1.p.rapidapi.com/oldenglish.json"
 // function clickHandler()
 // {
 // console.log("clicked ! ");
 // console.log("input ::", textInput.value);
 // };
 
+function getTranslatedURL(text)
+{
+    return serverURL+"?"+"text="+text;
+}
 
-function clickHandler(){outputBox.innerText = "ASDFG" + textInput.value
+function clickHandler(){
+    var inputT=textInput.value;
+    fetch(getTranslatedURL(inputT))
+    .then(Response=>Response.json)
+    .then(json=>console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler);
 
